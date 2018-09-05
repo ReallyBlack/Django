29e7e855dcd791b9be15c125b17a5e91344eae88
zhouxu <1141019356@qq.com>
2018-09-05 12:11:11 +0800

    use css change the html

--- Blog/urls.py
+++ Blog/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 
 urlpatterns = [
+    path('', views.blog_list, name='blog_list'),
     path('<int:blog_pk>', views.blog_detail, name='blog_detail'),
     path('type/<int:blog_in_type>', views.blogs_in_type, name='blogs_in_type'),
 ]
