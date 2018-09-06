40df85bb0a39f05785227aa0165acd1c705aa32b
zhouxu <1141019356@qq.com>
2018-09-06 18:14:23 +0800

    add read statistics

--- Demo/views.py
+++ Demo/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import render
 from Blog.models import Blog
 from django.contrib.contenttypes.models import ContentType
-from read_statistics.method_for_blog import get_a_week_read_date
+from read_statistics.method_for_blog import get_a_week_read_date, get_hot_blog
 
 
 def home(request):
     blog_content_type = ContentType.objects.get_for_model(Blog)
     dates, read_nums = get_a_week_read_date(blog_content_type)
+    today_hot_blog = get_hot_blog(blog_content_type)
     context = dict()
     context['read_nums'] = read_nums
     context['dates'] = dates
+    context['today_hot_blog'] = today_hot_blog
     return render(request, 'home.html', context)
--- read_statistics/method_for_blog.py
+++ read_statistics/method_for_blog.py
@@ -33,3 +33,9 @@ def get_a_week_read_date(content_type):
         result = read_details.aggregate(read_num_sum=Sum('read_num'))
         read_nums.append(result['read_num_sum']or 0)
     return dates, read_nums
+
+
+def get_hot_blog(content_type):
+    today = timezone.now().date()
+    read_details = ReadDetail.objects.filter(content_type=content_type, date=today).order_by('-read_num')
+    return read_details[0:3]
